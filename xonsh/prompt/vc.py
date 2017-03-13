163a0524af7bc529932e3ff46fa907f2db83c726
Ryan Gonzalez <rymg19@gmail.com>
2017-03-13 12:11:15 -0500

    Use a timeout instead of threads when detecting the Git branch

--- xonsh/prompt/vc.py
+++ xonsh/prompt/vc.py
@@ -12,45 +12,28 @@ import subprocess
 import xonsh.tools as xt
 
 
-def _get_git_branch(q):
-    try:
-        branches = xt.decode_bytes(subprocess.check_output(
-            ['git', 'branch'],
-            stderr=subprocess.DEVNULL
-        )).splitlines()
-    except (subprocess.CalledProcessError, OSError, FileNotFoundError):
-        q.put(None)
-    else:
-        for branch in branches:
-            if not branch.startswith('* '):
-                continue
-            elif branch.endswith(')'):
-                branch = branch.split()[-1][:-1]
-            else:
-                branch = branch.split()[-1]
-
-            q.put(branch)
-            break
-        else:
-            q.put(None)
-
-
 def get_git_branch():
     """Attempts to find the current git branch. If this could not
     be determined (timeout, not in a git repo, etc.) then this returns None.
     """
-    branch = None
     timeout = builtins.__xonsh_env__.get('VC_BRANCH_TIMEOUT')
-    q = queue.Queue()
 
-    t = threading.Thread(target=_get_git_branch, args=(q,))
-    t.start()
-    t.join(timeout=timeout)
     try:
-        branch = q.get_nowait()
-    except queue.Empty:
-        branch = None
-    return branch
+        branches = xt.decode_bytes(subprocess.check_output(
+            ['git', 'branch'],
+            stderr=subprocess.DEVNULL,
+            timeout=timeout,
+        )).splitlines()
+    except (subprocess.CalledProcessError, OSError, FileNotFoundError,
+            subprocess.TimeoutExpired):
+        return None
+    else:
+        for branch in branches:
+            if branch.startswith('* '):
+                branch = branch.split()[-1].rstrip(')')
+                return branch
+        else:
+            return None
 
 
 def _get_hg_root(q):
